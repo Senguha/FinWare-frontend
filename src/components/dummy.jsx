9757be72837c4f5d6fe744368268f933b1c3d318
Mtseng <darychev2013@yandex.ru>
2024-04-05 02:07:12 +0500

    .

--- src/components/dummy.jsx
+++ src/components/dummy.jsx
@@ -27,41 +27,39 @@ function Dummy() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center m-12">
-      <div className="w-3/4">
-        <div className="mb-6">
-          <ButtonGroup variant="outlined" aria-label="Basic button group">
-            <Button onClick={()=> fetchTable("companies")}>Предприятия</Button>
-            <Button onClick={()=> fetchTable("parametres")}>Показатели</Button>
-            <Button onClick={()=> fetchTable("parameteresTypes")}>Виды показателей</Button>
-            <Button onClick={()=> fetchTable("contactPersons")}>Контакные лица</Button>
-            <Button onClick={()=> fetchTable("countries")}>Страны</Button>
-            <Button onClick={()=> fetchTable("reports")}>Отчёты</Button>
-            <Button onClick={()=> fetchTable("users")}>Пользователи</Button>
-          </ButtonGroup>
+    <div className="flex flex-col items-center my-12 w-full px-6">
+        <div className="flex items-center mb-6 flex-wrap gap-1">
+            <Button variant="outlined" onClick={()=> fetchTable("parametres")}>Показатели</Button>
+            <Button variant="outlined" onClick={()=> fetchTable("companies")}>Предприятия</Button>
+            <Button variant="outlined" onClick={()=> fetchTable("parameteresTypes")}>Виды показателей</Button>
+            <Button variant="outlined" onClick={()=> fetchTable("contactPersons")}>Контакные лица</Button>
+            <Button variant="outlined" onClick={()=> fetchTable("countries")}>Страны</Button>
+            <Button variant="outlined" onClick={()=> fetchTable("reports")}>Отчёты</Button>
+            <Button variant="outlined" onClick={()=> fetchTable("users")}>Пользователи</Button>
         </div>
         <TableContainer component={Paper}>
           <Table>
             <TableHead>
-              {tableColumns.map((field) => {
-                return <TableCell>{field.name}</TableCell>;
+              <TableRow key={"0"}>
+              {tableColumns.map((field,i) => {
+                return <TableCell key={i}>{field.name}</TableCell>;
               })}
+              </TableRow>
             </TableHead>
             <TableBody>
               {tableRows.map((row) => {
                 return (
                   <TableRow key={row.id}>
-                    {Object.values(row).map((value) => (
-                      <TableCell>{value}</TableCell>
+                    {Object.values(row).map((value,i) => (
+                      <TableCell key={i}>{value}</TableCell>
                     ))}
                   </TableRow>
                 );
               })}
             </TableBody>
           </Table>
-        </TableContainer>
+        </TableContainer>   
       </div>
-    </div>
   );
 }
 
